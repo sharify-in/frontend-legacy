7f1f56e3fcb6e88c26562c41874ff48ffa543bb5
Creaperhunter <79284996+Creaperhunter@users.noreply.github.com>
2023-04-06 20:16:06 +0200

    Changed font and color palette

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,7 @@
 <script setup>
 import { RouterView } from "vue-router";
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap');
+</style>
