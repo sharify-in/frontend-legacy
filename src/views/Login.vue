f665dce3f68a5cf624a87e4cdd36c4883f084849
Creaperhunter <79284996+Creaperhunter@users.noreply.github.com>
2023-04-07 17:37:49 +0200

    Changed look of login page a little bit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,32 +2,54 @@
   <div
     class="flex flex-col justify-center items-center min-w-screen min-h-screen bg-[url('../svg/background.svg')] bg-no-repeat bg-cover p-5"
   >
-    <Card class="flex flex-col items-center w-full md:w-96">
-      <template #title>
-        <p class="text-center">Sign In</p>
-      </template>
-      <template #content>
-        <form action="">
-          <div class="flex flex-col gap-6">
-            <span class="p-float-label">
-              <InputText id="username" class="p-inputtext-lg" v-model="value" />
-              <label for="username">Username</label>
-            </span>
-            <span class="p-float-label">
-              <Password
-                v-model="pass"
-                class="p-inputtext-lg"
-                inputId="password"
-                :feedback="false"
-                toggleMask
+    <XyzTransition appear xyz="fade down duration-10 delay-1.5">
+      <Card v-if="true" class="flex flex-col items-center w-full md:w-96">
+        <template #title>
+          <p class="text-center">Sign In</p>
+        </template>
+        <template #content>
+          <form action="">
+            <div class="flex flex-col gap-6">
+              <div class="p-inputgroup flex-1">
+                <span class="p-inputgroup-addon">
+                  <i class="pi pi-user"></i>
+                </span>
+                <span class="p-float-label">
+                  <InputText
+                    id="username"
+                    class="p-inputtext-lg"
+                    v-model="value"
+                  />
+                  <label for="username">Username/Email</label>
+                </span>
+              </div>
+              <div class="p-inputgroup flex-1">
+                <span class="p-inputgroup-addon">
+                  <i class="pi pi-lock"></i>
+                </span>
+                <span class="p-float-label">
+                  <Password
+                    v-model="pass"
+                    class="p-inputtext-lg"
+                    inputId="password"
+                    :feedback="false"
+                    type="password"
+                    toggleMask
+                  />
+                  <label for="password">Password</label>
+                </span>
+              </div>
+              <Button
+                type="submit"
+                label="Sign In"
+                icon="pi pi-sign-in"
+                iconPos="right"
               />
-              <label for="password">Password</label>
-            </span>
-            <Button type="submit" label="Sign In" icon="pi pi-sign-in" iconPos="right" />
-          </div>
-        </form>
-      </template>
-    </Card>
+            </div>
+          </form>
+        </template>
+      </Card>
+    </XyzTransition>
   </div>
 </template>
 
@@ -37,3 +59,10 @@ import { ref } from "vue";
 const value = ref(null);
 const pass = ref(null);
 </script>
+
+<style scoped>
+.p-password > :global(input) {
+  border-top-left-radius: 0px !important;
+  border-bottom-left-radius: 0px !important;
+}
+</style>
