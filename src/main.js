8e0c4806c68f4b21e1e134604a1d866403005a47
Creaperhunter <79284996+Creaperhunter@users.noreply.github.com>
2023-04-10 23:38:35 +0200

    Added animations

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 
+import VueAnimXyz from '@animxyz/vue3'
+import '@animxyz/core' // Import css here if you haven't elsewhere
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -15,6 +18,7 @@ import "./assets/theme.css";
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VueAnimXyz)
 app.use(PrimeVue);
 app.use(router);
 
