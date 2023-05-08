50d85bf95d3a75c86430a1e246e4f8ce1918033c
Creaperhunter <creaperhunter531@gmail.com>
2023-05-08 18:31:58 +0200

    Countup animation in statistic component

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,43 +1,27 @@
 <template>
   <Card
-    class="min-w-[25%] flex-1 md:basis-80 hover:scale-105 hover:shadow-xl hover:shadow-almost-black duration-150 delay-75"
-  >
-    <template #title> <font-awesome-icon :icon="icon" /> {{ title }} </template>
+    class="min-w-[25%] flex-1 md:basis-80 hover:scale-105 hover:shadow-xl hover:shadow-almost-black duration-150 delay-75">
+    <template #title> <font-awesome-icon :icon="props.icon" /> {{ title }} </template>
     <template #content>
-      <p>
-        {{ formatNumber(number) }}
+      <p v-if="string">
+        {{ props.number }}
       </p>
+      <CountUp v-else :end-val="props.number"></CountUp>
     </template>
   </Card>
 </template>
 
-<script>
-export default {
-  props: {
-    icon: String,
-    title: String,
-    number: Number || String,
-  },
-  methods: {
-    formatNumber(num) {
-      if (typeof num === "string") {
-        return num;
-      }
+<script setup>
+import { onMounted, ref } from "vue"
+import CountUp from 'vue-countup-v3'
 
-      const scales = ["", "k", "m", "b", "t"];
-      const sign = Math.sign(num);
-      const absNum = Math.abs(num);
-      const scaleIndex = Math.min(
-        Math.floor(Math.log10(absNum) / 3),
-        scales.length - 1
-      );
-      const scale = scales[scaleIndex];
-      const scaledNum = absNum / Math.pow(10, scaleIndex * 3);
-      const formattedNum = scaledNum.toLocaleString(undefined, {
-        maximumFractionDigits: 1,
-      });
-      return sign * formattedNum + scale;
-    },
-  },
-};
+const props = defineProps(["icon", "title", "number"])
+
+let string = ref(false);
+onMounted(() => {
+  if (typeof props.number === "string") {
+    string.value = true
+  }
+})
+
 </script>
