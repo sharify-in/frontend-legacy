16451fd2c0aba1007cea7823598ec70f77fa76a1
Xiro <creaperhunter531@gmail.com>
2023-05-31 17:25:01 +0200

    Fixed registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,6 @@
 <template>
   <div
-    class="flex flex-col justify-center items-center min-w-screen min-h-screen bg-[url('../svg/background.svg')] bg-no-repeat bg-cover p-5"
-  >
+    class="flex flex-col justify-center items-center min-w-screen min-h-screen bg-[url('../svg/background.svg')] bg-no-repeat bg-cover p-5">
     <XyzTransition appear xyz="fade down duration-10 delay-1.5">
       <Card v-if="true" class="flex flex-col items-center w-full md:w-96">
         <template #title>
@@ -11,21 +10,14 @@
         </template>
         <template #content>
           <ProgressSpinner v-if="!request.done && !request.error" />
-          <form
-            v-if="request.done && !request.error"
-            @submit.prevent="register()"
-          >
+          <form v-if="request.done && !request.error" @submit.prevent="register()">
             <div class="flex flex-col gap-6">
               <div class="p-inputgroup flex-1">
                 <span class="p-inputgroup-addon">
                   <font-awesome-icon :icon="['fas', 'user']" />
                 </span>
                 <span class="p-float-label">
-                  <InputText
-                    id="username"
-                    class="p-inputtext-lg"
-                    v-model="formValues.username"
-                  />
+                  <InputText id="username" class="p-inputtext-lg" v-model="formValues.username" />
                   <label for="username">Username</label>
                 </span>
               </div>
@@ -34,11 +26,7 @@
                   <font-awesome-icon :icon="['fas', 'envelope']" />
                 </span>
                 <span class="p-float-label">
-                  <InputText
-                    id="email"
-                    class="p-inputtext-lg"
-                    v-model="formValues.email"
-                  />
+                  <InputText id="email" class="p-inputtext-lg" v-model="formValues.email" />
                   <label for="email">Email</label>
                 </span>
               </div>
@@ -47,13 +35,8 @@
                   <font-awesome-icon :icon="['fas', 'lock']" />
                 </span>
                 <span class="p-float-label">
-                  <Password
-                    v-model="formValues.password"
-                    class="p-inputtext-lg"
-                    inputId="password"
-                    type="password"
-                    toggleMask
-                  >
+                  <Password v-model="formValues.password" class="p-inputtext-lg" inputId="password" type="password"
+                    toggleMask>
                     <template #header>
                       <h6>Pick a password</h6>
                     </template>
@@ -66,35 +49,30 @@
                         <li>At least one numeric</li>
                         <li>Minimum 8 characters</li>
                       </ul>
-                    </template></Password
-                  >
+                    </template>
+                  </Password>
                   <label for="password">Password</label>
                 </span>
               </div>
-              <div class="p-inputgroup flex-1">
+              <div v-if="response.state.registration.inviteOnly" class="p-inputgroup flex-1">
                 <span class="p-inputgroup-addon">
                   <font-awesome-icon :icon="['fas', 'ticket']" />
                 </span>
                 <span class="p-float-label">
-                  <Password
-                    v-model="formValues.invite"
-                    class="p-inputtext-lg"
-                    inputId="invite"
-                    :feedback="false"
-                    type="password"
-                  />
+                  <Password v-model="formValues.invite" class="p-inputtext-lg" inputId="invite" :feedback="false"
+                    type="password" />
                   <label for="invite">invite</label>
                 </span>
               </div>
-              <Button
-                type="submit"
-                iconPos="right"
-                :loading="registerRequest.loading"
-              >
-              <div class="flex justify-center items-center w-full">
-                <font-awesome-icon :icon="['fas', 'user-plus']" />
-                <span class="px-2">Sign Up</span>
+              <div class="flex align-items-center">
+                <Checkbox v-model="formValues.agree" :binary="true" inputId="agree" name="agree" />
+                <label for="agree" class="ml-2"> I agree to the Terms of Service </label>
               </div>
+              <Button type="submit" iconPos="right" :loading="registerRequest.loading">
+                <div class="flex justify-center items-center w-full">
+                  <font-awesome-icon :icon="['fas', 'user-plus']" />
+                  <span class="px-2">Sign Up</span>
+                </div>
               </Button>
             </div>
           </form>
@@ -124,22 +102,25 @@ const formValues = reactive({
   email: "",
   password: "",
   invite: "",
+  agree: false
 });
 
 const request = reactive({
   done: false,
   error: false,
 });
+
+const response = ref(null)
 onMounted(async () => {
   await axios
     .get("/stats")
-    .then(() => {
+    .then((res) => {
+      response.value = res.data
       request.done = true;
     })
     .catch(() => {
       request.error = true;
     });
-  console.log(request);
 });
 
 const registerRequest = reactive({
@@ -148,14 +129,28 @@ const registerRequest = reactive({
   error: false,
 });
 const register = async () => {
-  await axios.post("/auth/register", form).then((res) => {
-    const user = res.data.user;
+  await axios.post("/auth/register", formValues).then((res) => {
+    toast.add({ severity: 'success', summary: 'Success!', detail: res.data.message, life: 3000 });
+    return router.push({ name: "login" })
+  }).catch((err) => {
+    const furry = err.response.data
+
+    if (furry.errors) {
+      registerRequest.loading = false
+      return toast.add({
+        severity: "error",
+        summary: "Validation Error",
+        detail: furry.errors[0].message,
+        life: 3000
+      })
+    }
+
   });
 };
 </script>
 
 <style scoped>
-.p-password > :global(input) {
+.p-password> :global(input) {
   border-top-left-radius: 0px !important;
   border-bottom-left-radius: 0px !important;
 }
