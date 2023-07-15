fd6090518ac4b806fbcc973e6f453fffb371a5d3
Xiro <xiro@duck.com>
2023-07-15 16:46:10 +0200

    Unfinished stuff

--- src/views/dashboard/@me.vue
+++ src/views/dashboard/@me.vue
@@ -1,54 +1,20 @@
 <template>
-    <div class="flex flex-col gap-3">
-        <div class="flex justify-between gap-5">
-            <a-card title="Username" class="w-1/3">
-                <!--
-                    BUG: Username cannot be edited. please fix 
-                -->
-                <UsernameChange />
-            </a-card>
-            
-            <a-card title="Profile" class="w-2/3">
-                <div class="flex flex-row gap-2 flex-wrap">
-
-                    <div class="flex flex-col grow">
-                        <div>
-                            <a-checkbox v-model:checked="checked">
-                                <h1 class="font-bold text-lg"></h1>
-                            </a-checkbox>
-                            <p>Setting description</p>
-                        </div>
-                    </div>
-
-                    <div class="flex flex-col grow">
-                        <div>
-                            <a-checkbox v-model:checked="checked">
-                                <h1 class="font-bold text-lg">Setting</h1>
-                            </a-checkbox>
-                            <p>Setting description</p>
-                        </div>
-                    </div>
-
-                    <div class="flex flex-col grow">
-                        <div>
-                            <a-checkbox v-model:checked="checked">
-                                <h1 class="font-bold text-lg">Setting</h1>
-                            </a-checkbox>
-                            <p>Setting description</p>
-                        </div>
-                    </div>
-                    
-                    <div class="flex flex-col grow">
-                        <div>
-                            <a-checkbox v-model:checked="checked">
-                                <h1 class="font-bold text-lg">Setting</h1>
-                            </a-checkbox>
-                            <p>Setting description</p>
-                        </div>
-                    </div>
-                    
-                </div>
-            </a-card>
-        </div>
-    </div>  
+    <div class="flex flex-row gap-3">
+        <a-card title="Profile" class="w-1/2">
+            <div class="flex flex-col items-center w-full h-full">
+                <a-avatar :src="avatarUrl" :size="{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }"/>
+                <h1 class="text-3xl mt-5">@{{ authStore.user.username }}</h1>
+                <p>{{ authStore.user.id }}</p>
+            </div>
+        </a-card>
+    </div>
 </template>
+
+<script setup>
+import { useAuthStore } from "@/stores/UserStore";
+
+const authStore = useAuthStore();
+
+const usernameLetter = authStore.user.username.slice(0, 2).toUpperCase();
+const avatarUrl = `https://avatar.vercel.sh/${authStore.user.username}.svg?text=${usernameLetter}`
+</script>
